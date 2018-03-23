0beb18daa1c1a4542670ab66b247594638104789
matthewtessler <matthewtessler96@gmail.com>
2018-03-23 16:37:21 -0400

    add finished midterm project

--- interactive/midterm-project/sketch.js
+++ interactive/midterm-project/sketch.js
@@ -2,11 +2,33 @@ var state; // true - play, false - pause
 var diff; // 0 - easy, 1 - medium, 2 - hard
 var points; // score-keeping
 var count; // what num of keys being played in sequence
-var playListenBuild; // 0 - play, 1 - listen , 2 - build
-var keys;
+var playListenBuildInform; // 0 - play, 1 - listen , 2 - build
+var keys; // stack of keys to play back, match off of
+var upperBounds; // easy, medium, hard upper bounds for range of ASCII codes
+var startMillis = -1; // timing mechanism, -1 indicates reset
+var pos; // when viewing stack/arr of keys, the pos is used to read backwards
+var winLoss; // flag indicates if user played sequence of keys correctly or not
+var osc; // oscillator for sounds
+var startOsc; // flag for osc start
+var waitMillis; // use for brief delay of keys to differentiate
+var letters; // to hold letter objects with images
+var startBg; // start background
+var pausedBg; // pause background
+var timesPaused; // to track how many times the game has been paused
 
 function getRandomInteger(min, max) { // random integer function from W3Schools.com
 	return Math.floor(Math.random() * (max - min + 1) ) + min; // both inclusive
+}
+
+function preload() {
+	letters = []; // letters arr will hold images
+	for (var i = 65; i < 91; i++) { // add image objects to arr
+		letters.push(new Letter(loadImage("letters/" + String(i) + ".png")));
+	}
+	// all external graphics created by Melissa Lopez, mel501@nyu.edu
+	startBg = loadImage("backgrounds/start.jpg"); // start bg
+	pausedBg = loadImage("backgrounds/paused.jpg"); // paused bg
+	bg = loadImage("backgrounds/bg.jpg"); // playing bg
 }
 
 function setup() {
@@ -16,83 +38,123 @@ function setup() {
 	diff = -1; // diff starts as -1, "not chosen"
 	points = 0; // user starts at 0 points
 	count = 1; // one key to repeat at first
-	playListenBuild = 2; // start in build mode (have to make the stack of keys)
+	playListenBuildInform = 2; // start in build mode (have to make the stack of keys)
 	keys = []; // start off with an empty stack
+	upperBounds = [68, 76, 90]; // set upper bounds for difficulty modes ASCII codes
+	osc = new p5.SinOsc(); // for sounds
+	startOsc = false; // flag for starting sounds
+	waitMillis = -1; // for artificial delay to distinguish characters
+	timesPaused = 0; // to track number of times the game has been paused/unpaused
 }
 
 function draw() {
 	if (state) { // play
-		console.log("hello!");
-		background(0) // bg black
-		fill(255); // text white
+		imageMode(CORNER); // puts image by corner point
+		background(bg) // bg black
+		fill(0); // text white
 
-		if (playListenBuild === 0) { // play back keys
-			text("Playing . . .", 25, 25);
-
-			// bing bom boom 
-
-			// this has to be time somehow so it doesn't go at 60 fps
-			// has to be one every 1-3 seconds
-
-			// first check if stack is not empty, if it is, change to listening
-			// if stack is not empty, pop one from stack, play it (and eventually it's note)
-
-		}
-		else if (playListenBuild === 1) { // listen u keys
-			text("Listening . . .", 25, 25);
-
-			// listen for sequence of keys (I must only register when key goes up so I don't have double counts)
-			// as the keys are being played, (eventually) play their notes
-
-			// i'd like to compare to stack in real time
-
-			// bing bom boom
-					// points += diff * count
-					// count++
-					// back into build mode
-				// bing bom bing
-					// wrong!
-					// count = 1
-					// start again
-					// back into build mode
-
-		}
-		else if (playListenBuild === 2) { // build set of keys
-			text("Building . . .", 25, 25);
-			// build stack with *count* number of keys randomly chosen
-			for (var i = 0 ; i < count; i++) {
-				keys.push(getRandomInteger(65,68));
+		if (playListenBuildInform === 0) { // play back keys
+			text("Playing . . .\nPoints: " + points, 30, 35); 
+			if (waitMillis === -1) { // insert artificial frame hiccup
+				waitMillis = millis(); // set new wait time
 			}
-			console.log(keys);
-			playListenBuild = 0; // once done building, switch to play mode
+			else if (waitMillis > 250) { // pause for 250 milliseconds so user can distinguish to identical keys
+				text(String(keys.length - pos) + " - " + String(String.fromCharCode(keys[pos])), 25, 75); // current key
+				letters[keys[pos] - 65].display(); // display the letter image
+				osc.freq(midiToFreq(keys[pos]-5)); // get freq for new key
 
-			// populate a stack with randomly chosen keys 
-			// the keys that can be possibly chosen depends on diff level
-			// easy -   ABCD
-			// medium - ABCDEFGHIJKL
-			// hard -   ABCDEFGHIJKLMNOPQRSTUVWXYZ
+				if (startMillis === -1) { // just entering play mode, set a fresh timer
+					startMillis = millis();
+					osc.start(); // start playin when timer starts
+				}
+				else if (millis() - startMillis > 1500) { // waited 1.5 seconds, go ahead
+					pos--; // play next lowest letter on stack
+					waitMillis = -1; // reset the wait time for the next time
+					startMillis = millis(); // reset timer
+					if (pos === -1) { // if there are no more elements to read on stack
+						playListenBuildInform = 1; // switch to listen mode
+						startMillis = -1; // reset for listen
+						osc.stop(); // stop when no more notes to play
+						pos = keys.length - 1; // start at end of arr again
+					}
+				}
+			}			
+		}
+		else if (playListenBuildInform === 1) { // listen for keys
+			text("Listening . . .\nPoints: " + points, 30, 35);		
+		}
+		else if (playListenBuildInform === 2) { // build set of keys
+			text("Building . . .\nPoints: " + points, 30, 35);
+			if (startMillis === -1) { // 
+				// keys = []; 
+				// for (var i = 0; i < count; i++) { // add a random key to keys stack
+					keys.unshift(getRandomInteger(65,upperBounds[diff]));
+				// }
+				startMillis = millis(); // wait 3000 milliseconds after this time
+			}
+			else if (millis() - startMillis > 3000) {
+				playListenBuildInform = 0; // once done building, switch to play mode
+				startMillis = -1; // reset startMillis to be used again in play mode
+				pos = count - 1; // position to read back keys at
+			}
+		}
+		else if (playListenBuildInform === 3) { // inform that they lost or won
+			text("Informing . . .\nPoints: " + points, 30,35);
+
+			// option to go into pauses mode and change diff?
+			if (startMillis === -1) {
+				startMillis = millis();
+			}
+			else if (millis() - startMillis < 3000) {
+				// if user won
+				if (winLoss) {
+					fill(0);
+					text("Correct! Back to building . . .",30,85);
+				}
+				// if user lost
+				else {
+					// you made it to [this many] number of keys
+					fill(0);
+					text("You lost. Restarting now with 1 key . . .",30,60);
+				}
+			}
+			else {
+				playListenBuildInform = 2; // to build mode
+				startMillis = -1; // restart clock
+			}	
+
+		
+			// stops tones from LISTEN section
+			if (millis() - startMillis > 1000) { 
+				osc.stop();
+			}
 		}
 	}
 	else { // pause
-		background(128); // bg gray
-		text("You are playing SimonKeys. You're current score is " + points + ".", 25,25);
+		imageMode(CORNER);
+		if (timesPaused > 0) {
+			background(pausedBg);
+		}
+		else {
+			background(startBg);
+		}
+
+		text("You are playing SimonKeys. You're current score is " + points + ".", 30,35);
 
 		if (diff === -1) { // no mode chosen
-			text("You have not chosen a difficulty mode.", 25, 50);
+			text("You have not chosen a difficulty mode.", 30, 60);
 		}
 		else if (diff === 0) { // easy mode chosen
-			text("You are currently in Easy Mode.", 25,50);
+			text("You are currently in Easy Mode.", 30,60);
 		}
 		else if (diff === 1) { // medium mode chosen
-			text("You are currently in Medium Mode.", 25, 50);
+			text("You are currently in Medium Mode.", 30, 60);
 		}
 		else if (diff === 2) { // hard mode chosen
-			text("You are currently in Hard Mode.", 25, 50);
+			text("You are currently in Hard Mode.", 30, 60);
 		}
-
-		text("For Easy (4 keys) select 1. For Medium (12 keys) select 2. For Hard (26 keys) select 3.", 25, 75);
-		text("To restart the game, choose a new difficulty mode. Click the space bar to play.", 25, 100);
-
+		text("For Easy (4 keys) select 1. For Medium (12 keys) select 2. For Hard (26 keys) select 3.", 30, 85);
+		text("Click the space bar to play. Must refresh browser to change difficulty mode.", 30, 110);
 	}
 } 
 
@@ -101,23 +163,51 @@ function keyPressed() {
 		if (diff !== -1) { // if user has difficulty mode chosen, let them play
 			state = !state;
 		}
+		timesPaused++; // if timesPaused is greater than 0 user cannot change difficulty mode
 	}
-	else if (keyCode === 49 && !state) {
-		if (diff !== 0) { // if selection is new, restart
-			diff = 0;
-			count = 1; // back to one key
-		}
+	else if (keyCode > 48 && keyCode < 52 && timesPaused === 0) {
+			diff = keyCode-49; // easy
+			count = 1; // start with one key
 	}
-	else if (keyCode === 50 && !state) {
-		if (diff !== 1) { // if selection is new, restart
-			diff = 1;
-			count = 1; // back to one key
+	else if (state && playListenBuildInform === 1) { // listening for key press response
+		text(String.fromCharCode(keyCode), 25, 75);
+		osc.freq(midiToFreq(keyCode-5)); // change frequency to match key
+		console.log("hello");
+
+		// makes sure osc only starts once so no repetitive sound
+		if (!startOsc) { 
+			osc.start();
+			startOsc = true;
 		}
+
+		if (keyCode !== keys[pos]) { // they lost
+			// reset
+			count = 1; // count back to one, user starts again
+			keys = []; // reset keys here
+			playListenBuildInform = 3;
+			winLoss = false; // tells inform that user lost
+			startOsc = false; // set osc back to not playing in LISTEN
+		}
+		else if (pos == 0){ // they won b/c they played all keys back
+			points += (upperBounds[diff] - 64) * count;
+			count++; // increment count to add an additional key to next round
+			playListenBuildInform = 3;
+			winLoss = true; // tells inform that user won
+			startOsc = false; // set osc back to not playing in LISTEN
+		}
+
+		pos--; // decrement pos to show next key when another is pressed
 	}
-	else if (keyCode === 51 && !state) {
-		if (diff !== 2) { // if selection is new, restart
-			diff = 2;
-			count = 1; // back to one key
-		}
+}
+
+// letter class will hold images and allow display
+class Letter {
+	constructor(img) { // add img object
+		this.img = img;
+	}
+
+	display() { // display at 250,250
+		imageMode(CENTER);
+		image(this.img, 250,300);
 	}
 }
